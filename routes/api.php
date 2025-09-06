3e660e1be77078c4ef49750aac437255e9d66e87
Rabican-code <rabikantsingh24@gmail.com>
2025-09-06 12:48:35 +0530

    API fix

--- routes/api.php
+++ routes/api.php
@@ -6,22 +6,21 @@ use App\Http\Controllers\AppController;
 use App\Http\Controllers\ImageController;
 
 
-Route::middleware('api')->get('/categories', [ApiController::class, 'categories']);
-Route::middleware('api')->get('/stores', [ApiController::class, 'stores']);
-Route::middleware('api')->get('/categories/{id}/stores', [ApiController::class, 'storesByCategory']);
-Route::middleware('api')->get('/stores/{id}/products', [ApiController::class, 'storeProducts']);
-Route::middleware('api')->get('/orders', [ApiController::class, 'getOrders']);
-Route::middleware('api')->get('/search', [ApiController::class, 'search']);
-Route::middleware('api')->get('/images/{type}/{filename}', [ImageController::class, 'show'])->where('filename', '.*');
+Route::get('/categories', [ApiController::class, 'categories']);
+Route::get('/stores', [ApiController::class, 'stores']);
+Route::get('/categories/{id}/stores', [ApiController::class, 'storesByCategory']);
+Route::get('/stores/{id}/products', [ApiController::class, 'storeProducts']);
+Route::get('/orders', [ApiController::class, 'getOrders']);
+Route::get('/search', [ApiController::class, 'search']);
+Route::get('/images/{type}/{filename}', [ImageController::class, 'show'])->where('filename', '.*');
 Route::get('/drivers', [AppController::class, 'getDriver']);
-
-
 Route::get('/users', [ApiController::class, 'getUsers']);
 Route::get('/user/current', [ApiController::class, 'getCurrentUser']);
-Route::middleware('api')->post('/create-products', [ApiController::class, 'addProduct']);
-Route::middleware('api')->post('/create-stores', [ApiController::class, 'addStore']);
-Route::middleware('api')->post('/create-categories', [ApiController::class, 'addCategory']);
-Route::middleware('api')->post('/create-orders', [ApiController::class, 'addOrder']);
+
+Route::post('/create-products', [ApiController::class, 'addProduct']);
+Route::post('/create-stores', [ApiController::class, 'addStore']);
+Route::post('/create-categories', [ApiController::class, 'addCategory']);
+Route::post('/create-orders', [ApiController::class, 'addOrder']);
 Route::post('/creat-drivers', [AppController::class, 'addDriver']);
 
 
